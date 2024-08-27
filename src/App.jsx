e5ec7c5549bb18066fffdf0adfb941a6e2a210de
Nageshkumar123 <105060008+Nageshkumar123@users.noreply.github.com>
2024-08-27 12:01:17 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css';
+import PassWordGenrator from './Components/PassWordGenrator';
+
+export default function App() {
+  return (
+    <div>
+   
+   
+<PassWordGenrator/>
+   </div>
+  )
+}
---
+++ src/NavBar.jsx
@@ -0,0 +1,11 @@
+import logo from '../MovieImage.png'
+const NavBar=()=>{
+return (
+    <div>
+     <img src={logo} alt="not found"></img>  
+     <a href='/'>Home</a>
+     
+    </div>
+)
+}
+export default NavBar;
